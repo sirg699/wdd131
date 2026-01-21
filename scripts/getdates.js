301e4e901a0aada73645d112d1b3072fa3436150
sirg699 <kiseh@byupathway.edu>
2026-01-21 12:05:16 +0100

    week 1

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+
+const yearSpan = document.getElementById("currentyear");
+yearSpan.textContent = new Date().getFullYear();
+
+document.getElementById("lastModified").innerHTML = "Last Modified: " + document.lastModified;
