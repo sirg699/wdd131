cee340e9aad819489c2efadff0d3d3cbf8f6685e
sirg699 <kiseh@byupathway.edu>
2026-02-11 12:25:43 +0100

    week 5

---
+++ scripts/form.js
@@ -0,0 +1,56 @@
+const products = [
+  {
+    id: "fc-1888",
+    name: "flux capacitor",
+    averagerating: 4.5
+  },
+  {
+    id: "fc-2050",
+    name: "power laces",
+    averagerating: 4.7
+  },
+  {
+    id: "fs-1987",
+    name: "time circuits",
+    averagerating: 3.5
+  },
+  {
+    id: "ac-2000",
+    name: "low voltage reactor",
+    averagerating: 3.9
+  },
+  {
+    id: "jj-1969",
+    name: "warp equalizer",
+    averagerating: 5.0
+  }
+];
+
+const productSelect = document.getElementById("product");
+
+if (productSelect) {
+  products.forEach(product => {
+    const option = document.createElement("option");
+    option.value = product.id;    
+    option.textContent = product.name; 
+    productSelect.appendChild(option);
+  });
+}
+
+if (window.location.pathname.includes("review.html")) {
+
+  let reviewCount = localStorage.getItem("reviewCount");
+
+  if (reviewCount === null) {
+    reviewCount = 0;
+  }
+
+  reviewCount++;
+
+  localStorage.setItem("reviewCount", reviewCount);
+
+  const reviewDisplay = document.getElementById("reviewTotal");
+  if (reviewDisplay) {
+    reviewDisplay.textContent = reviewCount;
+  }
+}
